238f359cc99d00c3b2c68509c6fafbed76cddaeb
Rockey <bjxdrj@gmail.com>
2024-05-14 12:57:58 +0800

    优化

--- shells/deploy.sh
+++ shells/deploy.sh
@@ -1,6 +1,14 @@
-# 清楚缓存
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../hexo-theme-keep-starter || exit 1 
+# 清除缓存
 hexo clean
 # 生成文件
-hexo g
-# 部署到服务上
-hexo d
+hexo generate
+# 移动文件
+cp -r ./public/* ../doc
+cp ./_config.yml ../_config.yml
+cp -r ./source/ ../source/
+# 手动提交
+cd ../
+git add .
+git commit --amend -m "Add Hexo static files to doc folder"
+git push --force origin master
